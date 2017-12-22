e1710b4bf78d6b234f97bf23ed7c9206e49fd667
ravindersingh17 <n30.2006@gmail.com>
2017-12-22 14:41:31 +0530

    Missing file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+setup(
+        name="git2deploy",
+        version="1.0",
+        description="Deploy software from git to dir",
+        author="Ravinder Singh",
+        author_email="ravinder.ssgh@gmail.com",
+        url="https://github.com/ravindersingh17/git2deploy",
+        python_requires=">=3",
+        packages=["g2d"],
+        scripts=["bin/g2d"],
+        )
+
