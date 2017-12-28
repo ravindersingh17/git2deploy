84e87f4dc3b02a6e6d4bb2b2c929e4ed531954ba
ravindersingh17 <n30.2006@gmail.com>
2017-12-28 13:04:31 +0530

    tpb recipient

--- hangouts/ravisbot/simpleecho.py
+++ hangouts/ravisbot/simpleecho.py
@@ -17,3 +17,9 @@ class webhookReceiver(AsyncRequestHandler):
             if dev_convs:
                 for conv in dev_convs:
                     yield from self.bot.coro_send_message(conv, parsed_content["message"])
+
+        elif parsed_content["to"] == "tpb":
+            yield from self.bot.coro_send_message(parsed_content["tpbrecipient"], parsed_content["message"])
+
+        else:
+            pass
